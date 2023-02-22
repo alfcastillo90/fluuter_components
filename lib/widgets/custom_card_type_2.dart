9b471c581f04a253c16875ebc199968c5bfa276b
Carlos Alfredo Castillo Rodriguez <cacr1990@gmail.com>
2023-02-22 14:22:34 -0300

    ADD: widget con imagen

--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/theme/app_theme.dart';
 
 class CustomCardType2 extends StatelessWidget {
   const CustomCardType2({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+        clipBehavior: Clip.antiAlias,
+        elevation: 30,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+        shadowColor: AppTheme.primary.withOpacity(0.5),
+        child: Column(children: [
+          //landscape image
+          const FadeInImage(
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            image: NetworkImage(
+                'https://lacgeo.com/sites/default/files/la_gran_sabana_venezuela_opt.jpg'),
+            width: double.infinity,
+            height: 230,
+            fit: BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 300),
+          ),
+          Container(
+            alignment: AlignmentDirectional.centerEnd,
+            padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+            child: const Text('Landscape'),
+          ),
+        ]));
   }
 }
